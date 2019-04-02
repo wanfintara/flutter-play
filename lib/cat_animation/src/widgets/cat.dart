0792425ffbdbc28e52d51da963ac3e0fb7d894f6
wanfintara <wanfintara@gmail.com>
2019-04-02 23:58:12 +0700

    Asset: added cat.png

--- lib/cat_animation/src/widgets/cat.dart
+++ lib/cat_animation/src/widgets/cat.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class Cat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.network('https://i.imgur.com/QwhZRyL.png');
+    return Image.asset('cat.png');
   }
 }
